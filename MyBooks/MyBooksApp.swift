58b705cf37e72af1c1a8ac643e7331674a9acc11
Jacek Kosinski <kashiash@gmail.com>
2023-10-11 10:26:59 +0200

    Initial Commit

---
+++ MyBooks/MyBooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBooksApp.swift
+//  MyBooks
+//
+//  Created by Jacek Kosinski U on 11/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct MyBooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

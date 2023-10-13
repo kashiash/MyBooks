f7ae1ea980158ec55995b82d0b0b808e235eb618
Jacek Kosinski <kashiash@gmail.com>
2023-10-13 10:40:12 +0200

    CRUD skonczone

---
+++ MyBooks/EditBookView.swift
@@ -0,0 +1,18 @@
+//
+//  EditBookView.swift
+//  MyBooks
+//
+//  Created by Jacek Kosinski U on 12/10/2023.
+//
+
+import SwiftUI
+
+struct EditBookView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditBookView()
+}
